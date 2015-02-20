6cb5057bcab0d7df20d3343b5b0f8362c7f734e6
John DeSilva <desilvjo@umich.edu>
2015-02-20 18:51:22 -0500

    Initial commit

---
+++ src/main/scala/com/aesthetikx/good_hacker_news/activity/MainActivity.scala
@@ -0,0 +1,12 @@
+package com.aesthetikx.good_hacker_news.activity
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity {
+
+  override def onCreate(savedInstanceState: Bundle) {
+
+  }
+
+}
