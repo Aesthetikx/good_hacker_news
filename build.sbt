dacc8bf41e789f3ded8194b738cde3f37c3af05c
John DeSilva <desilvjo@umich.edu>
2015-02-20 20:31:25 -0500

    Add initial RecyclerView with and library dependency

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ android.Plugin.androidBuild
 
 platformTarget in Android := "android-21"
 
+libraryDependencies += "com.android.support" % "recyclerview-v7" % "21.0.3"
+
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "test"
 
 libraryDependencies += "com.squareup.retrofit" % "retrofit" % "1.9.0"
--- src/main/scala/com/aesthetikx/good_hacker_news/activity/MainActivity.scala
+++ src/main/scala/com/aesthetikx/good_hacker_news/activity/MainActivity.scala
@@ -2,11 +2,18 @@ package com.aesthetikx.good_hacker_news.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
 
 class MainActivity extends Activity {
 
   override def onCreate(savedInstanceState: Bundle) {
+    val view: RecyclerView = new RecyclerView(this)
 
+    view.setHasFixedSize(true)  // Size of row views won't change
+
+    view.setLayoutManager(new LinearLayoutManager(this))
+
+    setContentView(view)
   }
 
 }
