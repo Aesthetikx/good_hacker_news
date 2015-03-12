654dd45e5ffaddb534aa5af8eaf2138e4d4e3f6a
John DeSilva <desilvjo@umich.edu>
2015-03-11 20:53:42 -0400

    Make Story serializable

--- src/main/scala/com/aesthetikx/good_hacker_news/model/Story.scala
+++ src/main/scala/com/aesthetikx/good_hacker_news/model/Story.scala
@@ -2,4 +2,4 @@ package com.aesthetikx.good_hacker_news.model
 
 import java.net.URL
 
-case class Story(id: Int, title: String, url: URL, time: Long, score: Int, by: String, kids: Array[Int])
+case class Story(id: Int, title: String, url: URL, time: Long, score: Int, by: String, kids: Array[Int]) extends Serializable
